01dd8e8408909454491176f2a868d12a1c53c29f
sELLC0de <thesellstudio@gmail.com>
2025-04-14 23:33:03 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
 main_menu.add(
